da039e4342aab236c890899f2629c49a4a00b7e6
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-30 03:00:22 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title Onus Bot
+title Onus Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    Onus BOT SETUP AND RUN SCRIPT
+echo    Onus BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
